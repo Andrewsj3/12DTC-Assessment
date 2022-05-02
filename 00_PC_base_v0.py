29191e114d58652a23f1995141543afe48e64213
Andrewsj3 <andrewsj3@middleton.school.nz>
2022-05-02 17:37:42 +1200

    Initial template for main program

---
+++ 00_PC_base_v0.py
@@ -0,0 +1,40 @@
+"""Program Version
+Description
+Jack Andrews
+Date
+"""
+
+# global imports
+
+# functions
+
+# Main routine begins
+
+# Ask user for budget
+
+# Ask for unit
+
+# Enter main loop
+
+# Ask for product name
+
+# Ask for product mass/volume
+
+# Ask for price of product
+
+# Calculate price per unit
+
+# Store details of product: name, weight, price
+
+# Ask user if they want to compare another product, if not, break from loop
+
+# Sort list by unit price
+
+# Show all items within budget and all items outside budget
+
+# Ask user if they want to compare more items, if so, go to top of main routine
+
+# End of main routine
+
+# if __name__ == "__main__":
+# main()
